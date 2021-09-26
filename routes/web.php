97939543c4ce3372159a52381e7225b1c207e984
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2021-09-26 19:20:02 +0330

    add some routes and define blades and funcitons

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get("/", [HomeController::class, "index"]);
+Route::get("/post/{id}", [PostController::class, "index"]);
+Route::get("/cat/{id}", [CategoryController::class, "index"]);
