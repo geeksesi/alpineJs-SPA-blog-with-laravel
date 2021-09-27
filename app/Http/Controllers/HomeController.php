b18a611fa46f683ffd67eee8acd54fa722018f1a
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2021-09-27 17:29:18 +0330

    some dirty query :)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -10,7 +11,14 @@ class HomeController extends Controller
     public function index(Request $request)
     {
         $categories = Category::all();
-        // dd($categories);
-        return view("home", ["categories" => $categories]);
+        $latest = Post::orderBy("created_at")->paginate(5);
+        $popular = Post::inRandomOrder()
+            ->limit(5)
+            ->get();
+        return view("home", [
+            "categories" => $categories,
+            "latest" => $latest,
+            "popular" => $popular,
+        ]);
     }
 }
