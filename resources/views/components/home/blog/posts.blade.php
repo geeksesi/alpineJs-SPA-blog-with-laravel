13315b2737c5b1348ffae1fa23c194b175529c7b
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2021-09-27 17:31:49 +0330

    dynamic data

--- resources/views/components/home/blog/posts.blade.php
+++ resources/views/components/home/blog/posts.blade.php
@@ -1,97 +1,20 @@
-<div class="wrapper-side-left-blog flex overflow-x-scroll scroll-slider sm:flex-wrap gap-7 sm:w-2/3">
-    <div class="item-blog flex flex-col sm:flex-row gap-6 min-w-240">
-      <div class="img-item-blog sm:w-1/2 rounded-lg overflow-hidden">
-        <img class="object-cover object-center w-full h-full transform scale-110" src="img/blog1.png" alt="blog">
-      </div>
-      <div class="content-blog sm:w-1/2">
-        <h3 class="text-lg"><a href="#">13 things i’d Tell Any New Travler</a></h3>
-        <div class="author-post my-3 text-sm font-semibold"><span><a class="underline text-yellow-400"
-              href="#">Post</a></span><span class="by"> By </span><span>Adam Smith</span></div>
-        <div class="date-post text-sm">10 Nov, 2020 ----- <span class="text-yellow-400"><a href="#">50
-              comment</a></span></div>
-        <div class="caption-blog my-6">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit. In sed et donec purus viverra. Sit justo velit, eu sed sollicitudin tempus,
-          </p>
-        </div>
-        <div class="read-more text-sm text-yellow-400 font-semibold"><a href="#">READ MORE...</a></div>
-      </div>
+@foreach ($posts as $post)
+<div class="item-blog flex flex-col sm:flex-row gap-6 min-w-240 max-h-64">
+    <div class="img-item-blog sm:w-1/2 rounded-lg overflow-hidden">
+        <a href="{{route("post", ["slug"=> $post->slug])}}"><img class="object-cover object-center w-full h-full transform scale-110"
+            src="{{Storage::url($post->thumbnail)}}" alt="blog"></a>
     </div>
-    <div class="item-blog flex flex-col sm:flex-row gap-6 min-w-240">
-      <div class="img-item-blog sm:w-1/2 rounded-lg overflow-hidden">
-        <img class="object-cover object-center w-full h-full transform scale-110" src="img/blog2.png" alt="blog">
-      </div>
-      <div class="content-blog sm:w-1/2">
-        <h3 class="text-lg"><a href="#">13 things i’d Tell Any New Travler</a></h3>
-        <div class="author-post my-3 text-sm font-semibold"><span><a class="underline text-yellow-400"
-              href="#">Post</a></span><span class="by"> By </span><span>Adam Smith</span></div>
-        <div class="date-post text-sm">10 Nov, 2020 ----- <span class="text-yellow-400"><a href="#">50
-              comment</a></span></div>
+    <div class="content-blog sm:w-1/2">
+        <h3 class="text-lg"><a href="{{route("post", ["slug"=> $post->slug])}}">{{$post->title}}</a></h3>
+        <div class="date-post text-sm">{{$post->created_at->format('d M, Y')}}</div>
         <div class="caption-blog my-6">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit. In sed et donec purus viverra. Sit justo velit, eu sed sollicitudin tempus,
-          </p>
+            <p>
+                {{$post->brief()}}
+            </p>
         </div>
-        <div class="read-more text-sm text-yellow-400 font-semibold"><a href="#">READ MORE...</a></div>
-      </div>
-    </div>
-    <div class="item-blog flex flex-col sm:flex-row gap-6 min-w-240">
-      <div class="img-item-blog sm:w-1/2 rounded-lg overflow-hidden">
-        <img class="object-cover object-center w-full h-full transform scale-110" src="img/blog3.png" alt="blog">
-      </div>
-      <div class="content-blog sm:w-1/2">
-        <h3 class="text-lg"><a href="#">13 things i’d Tell Any New Travler</a></h3>
-        <div class="author-post my-3 text-sm font-semibold"><span><a class="underline text-yellow-400"
-              href="#">Post</a></span><span class="by"> By </span><span>Adam Smith</span></div>
-        <div class="date-post text-sm">10 Nov, 2020 ----- <span class="text-yellow-400"><a href="#">50
-              comment</a></span></div>
-        <div class="caption-blog my-6">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit. In sed et donec purus viverra. Sit justo velit, eu sed sollicitudin tempus,
-          </p>
-        </div>
-        <div class="read-more text-sm text-yellow-400 font-semibold"><a href="#">READ MORE...</a></div>
-      </div>
-    </div>
-    <div class="item-blog flex flex-col sm:flex-row gap-6 min-w-240">
-      <div class="img-item-blog sm:w-1/2 rounded-lg overflow-hidden">
-        <img class="object-cover object-center w-full h-full transform scale-110" src="img/blog4.png" alt="blog">
-      </div>
-      <div class="content-blog sm:w-1/2">
-        <h3 class="text-lg"><a href="#">13 things i’d Tell Any New Travler</a></h3>
-        <div class="author-post my-3 text-sm font-semibold"><span><a class="underline text-yellow-400"
-              href="#">Post</a></span><span class="by"> By </span><span>Adam Smith</span></div>
-        <div class="date-post text-sm">10 Nov, 2020 ----- <span class="text-yellow-400"><a href="#">50
-              comment</a></span></div>
-        <div class="caption-blog my-6">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit. In sed et donec purus viverra. Sit justo velit, eu sed sollicitudin tempus,
-          </p>
-        </div>
-        <div class="read-more text-sm text-yellow-400 font-semibold"><a href="#">READ MORE...</a></div>
-      </div>
-    </div>
-    <div class="item-blog flex flex-col sm:flex-row gap-6 min-w-240">
-      <div class="img-item-blog sm:w-1/2 rounded-lg overflow-hidden">
-        <img class="object-cover object-center w-full h-full transform scale-110" src="img/blog5.png" alt="blog">
-      </div>
-      <div class="content-blog sm:w-1/2">
-        <h3 class="text-lg"><a href="#">13 things i’d Tell Any New Travler</a></h3>
-        <div class="author-post my-3 text-sm font-semibold"><span><a class="underline text-yellow-400"
-              href="#">Post</a></span><span class="by"> By </span><span>Adam Smith</span></div>
-        <div class="date-post text-sm">10 Nov, 2020 ----- <span class="text-yellow-400"><a href="#">50
-              comment</a></span></div>
-        <div class="caption-blog my-6">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit. In sed et donec purus viverra. Sit justo velit, eu sed sollicitudin tempus,
-          </p>
-        </div>
-        <div class="read-more text-sm text-yellow-400 font-semibold"><a href="#">READ MORE...</a></div>
-      </div>
+        <div class="read-more text-sm text-yellow-400 font-semibold"><a
+                href="{{route("post", ["slug"=> $post->slug])}}">READ MORE...</a></div>
     </div>
 </div>
+
+@endforeach
