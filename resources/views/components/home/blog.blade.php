cf068219121a5fc6d09eb84af93880d83207e878
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2021-09-27 17:31:01 +0330

    refactor blog

--- resources/views/components/home/blog.blade.php
+++ resources/views/components/home/blog.blade.php
@@ -1,8 +1,15 @@
 <section id="blog">
     <h2 class="text-center text-lg sm:text-2xl mt-12 mb-5">My Lastest Blog</h2>
     <div class="wrapper-blog flex gap-5 mw-1080">
-        <x-home.blog.posts/>
-        <x-home.blog.contact/>
+        <div class="wrapper-side-left-blog flex content-start overflow-x-scroll scroll-slider sm:flex-wrap gap-7 sm:w-2/3">
+            <x-home.blog.posts :posts="$latest"/>
+        </div>
+        <div class="wrapper-side-right-blog contact hidden md:flex flex-col gap-5 w-1/3">
+            <x-home.blog.popular-posts :posts="$popular"/>
+            <x-home.blog.social/>
+            <x-home.blog.subscribe/>
+            <x-home.blog.tags/>
+        </div>
     </div>
 </section>
 
