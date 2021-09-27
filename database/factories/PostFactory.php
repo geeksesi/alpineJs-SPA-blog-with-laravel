68e56399c6b64f00a93bdd068b4f2d862eec2928
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2021-09-27 17:31:26 +0330

    change title with sentence

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -22,7 +22,7 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
-            "title" => $this->faker->title(),
+            "title" => $this->faker->sentence(),
             "content" => $this->faker->text(),
             "category_id" => rand(1, 10),
             "thumbnail" =>
