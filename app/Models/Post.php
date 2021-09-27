2da8857eb42ae76b4ad2e9a7a476e4f6187c09c0
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2021-09-27 17:29:25 +0330

    add brief function

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -25,4 +25,20 @@ class Post extends Model
     {
         return "slug";
     }
+
+    public function brief()
+    {
+        $text = strip_tags($this->content);
+        if (strlen($text) > 150) {
+            // truncate string
+            $textCut = substr($text, 0, 150);
+            $endPoint = strrpos($textCut, " ");
+
+            //if the string doesn't contain any space then it will cut without word basis.
+            $text = $endPoint
+                ? substr($textCut, 0, $endPoint)
+                : substr($textCut, 0);
+        }
+        return $text;
+    }
 }
