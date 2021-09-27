28cf246bbc6c6230a26aa264090031327ff9e0d5
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2021-09-27 14:46:12 +0330

    now we have dynamic categories

--- resources/views/components/home/categories.blade.php
+++ resources/views/components/home/categories.blade.php
@@ -1,49 +1,30 @@
 <section id="category">
     <h2 class="text-lg sm:text-2xl mt-12 mb-5 text-center">Choose A Category</h2>
     <div id="slider-category" class="swiper-container mw-1080 pb-12 relative overflow-hidden">
-      <!-- Additional required wrapper -->
-      <div class="swiper-wrapper">
-        <!-- Slides -->
-        <div class="swiper-slide overflow-hidden rounded-lg"><img
-            class="mx-auto transform hover:scale-125 transition duration-300" src="img/category1.png"
-            alt="slide category">
-          <p class="absolute top-1/2 w-full text-center text-white text-lg -mt-3">Solo Travel</p>
+        <!-- Additional required wrapper -->
+        <div class="swiper-wrapper">
+            <!-- Slides -->
+            @foreach ($categories as $category)
+            <div class="swiper-slide overflow-hidden rounded-lg">
+                <a href="{{route("category", ["slug"=>$category->slug])}}">
+                    <img class="mx-auto transform hover:scale-125 transition duration-300"
+                        src="{{empty($category->thumbnail)?asset("img/category1.png"):storage::url($category->thumbnail)}}"
+                        alt="{{$category->name}}">
+                    <p class="absolute top-1/2 w-full text-center text-white text-lg -mt-3">{{$category->name}}</p>
+                </a>
+            </div>
+            @endforeach
+
         </div>
-        <div class="swiper-slide overflow-hidden rounded-lg"><img
-            class="mx-auto transform hover:scale-125 transition duration-300" src="img/category2.png"
-            alt="slide category">
-          <p class="absolute top-1/2 w-full text-center text-white text-lg -mt-3">Mount Travel</p>
+        <!-- pagination -->
+        <div class="swiper-pagination"></div>
+        <!-- navigation buttons -->
+        <div>
+            <img class="swiper-button-prev transform rotate-180 bg-yellow-400 rounded-full w-8 h-9" src="img/Right.svg"
+                alt="prev">
         </div>
-        <div class="swiper-slide overflow-hidden rounded-lg"><img
-            class="mx-auto transform hover:scale-125 transition duration-300" src="img/category3.png"
-            alt="slide category">
-          <p class="absolute top-1/2 w-full text-center text-white text-lg -mt-3">Jungle Travel</p>
+        <div>
+            <img class="swiper-button-next bg-yellow-400 rounded-full w-8 h-9" src="img/Right.svg" alt="next">
         </div>
-        <div class="swiper-slide overflow-hidden rounded-lg"><img
-            class="mx-auto transform hover:scale-125 transition duration-300" src="img/category4.png"
-            alt="slide category">
-          <p class="absolute top-1/2 w-full text-center text-white text-lg -mt-3">Road Travel</p>
-        </div>
-        <div class="swiper-slide overflow-hidden rounded-lg"><img
-            class="mx-auto transform hover:scale-125 transition duration-300" src="img/category5.png"
-            alt="slide category">
-          <p class="absolute top-1/2 w-full text-center text-white text-lg -mt-3">Osean Travel</p>
-        </div>
-        <div class="swiper-slide overflow-hidden rounded-lg"><img
-            class="mx-auto transform hover:scale-125 transition duration-300" src="img/category6.png"
-            alt="slide category">
-          <p class="absolute top-1/2 w-full text-center text-white text-lg -mt-3">Old CIty Travel</p>
-        </div>
-      </div>
-      <!-- pagination -->
-      <div class="swiper-pagination"></div>
-      <!-- navigation buttons -->
-      <div>
-        <img class="swiper-button-prev transform rotate-180 bg-yellow-400 rounded-full w-8 h-9" src="img/Right.svg"
-          alt="prev">
-      </div>
-      <div>
-        <img class="swiper-button-next bg-yellow-400 rounded-full w-8 h-9" src="img/Right.svg" alt="next">
-      </div>
     </div>
-  </section>
+</section>
