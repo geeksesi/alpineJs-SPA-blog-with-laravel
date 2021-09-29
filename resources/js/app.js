c1ae66b46b65e55692b821d15364552494e82f3a
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2021-09-29 11:05:46 +0330

    i don't need bootstrap. i should use my framework

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-require('./bootstrap');
+import Alpine from "alpinejs";
+import { AlpinePlugins } from "./Morakab";
+
+AlpinePlugins.forEach((plugin) => {
+    Alpine.plugin(plugin);
+});
+
+window.Alpine = Alpine;
+Alpine.start();
